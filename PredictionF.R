9a236f3ef1f718401bf9e4d0d56f3a7710c74412
alr94AR <alan.n.rashid@gmail.com>
2019-05-23 19:46:09 +0200

    rozpoczęte prace nad funkcją predykcji

---
+++ PredictionF.R
@@ -0,0 +1,9 @@
+myxgb_predict <- function (model, data){#funkcja predykcji do u¿ycia przy naszym modelu
+  
+  labels <- numeric(dim(data)[1])
+  for (i in 1:#uzupe³niæ){
+       
+       labels += #predykcja rpartu na podstawie danego modelu razy wagi tego modelu
+  }
+  
+}
