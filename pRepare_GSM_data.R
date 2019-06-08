89fd570d629d7d438e4e4c9c2834315919989a05
akalino1 <arek.kalinowski.94@gmail.com>
2019-06-08 22:45:05 +0200

    obróbka GSM

--- pRepare_GSM_data.R
+++ pRepare_GSM_data.R
@@ -5,9 +5,14 @@ gsmdata = read.delim("datasets/gsm/raw/orange_small_test.csv")
 lab = read.delim("datasets/gsm/raw/orange_small_train_appetency.labels.csv", header = FALSE)
 
 tset <-list(1)
-tset <- gsmdata[1]
+tset <- gsmdata
 tlab <-list(1)
 tlab <- lab[1]
+
+tset["appetency_labels"] <- read.delim("datasets/gsm/raw/orange_small_train_appetency.labels.csv", header = FALSE)[1]
+tset["upselling_labels"] <- read.delim("datasets/gsm/raw/orange_small_train_upselling.labels.csv", header = FALSE)[1]
+
+targets <- list(appetency_labels~.-upselling_labels, upselling_labels~.-appetency_labels)
 
 j <- 1
 # Lista tych atrybutow liczba rekordow "NA" jest mniejsza od 90% wszystkich rekordow
@@ -17,7 +22,6 @@ for (i in 1:length(names(gsmdata))) {
   if(s<0.9) {tset[j] <- gsmdata[i];j=j+1}
 }
 # przerobienie -1 na 0 w lab, zapisanie tego do tlab - uzywany do obliczen
-for (i in 1:length(lab$V1)) {
-  if(lab$V1[i] == -1)   {tlab[i] <- 0}
-  else                  {tlab[i] <- 1}
-}
+tset["upselling_labels"] <- (tset["upselling_labels"]+1)/2
+tset["appetency_labels"] <- (tset["appetency_labels"]+1)/2
+
