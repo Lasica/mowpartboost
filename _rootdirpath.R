61d87cf2a9073c977b7cd6495f2388ec6e3b8cbb
alr94AR <alan.n.rashid@gmail.com>
2019-06-09 17:53:14 +0200

    msg

---
+++ _rootdirpath.R
@@ -0,0 +1,10 @@
+#<<<<<<< HEAD:rootdirpath.R
+#AKpath <- "/Users/arek/Documents/GitHub/mowpartboost/"
+ARpath <- "C:/Studia MGR/Semestr II/MOW/mowpartboostmain/mowpartboost"
+rootdirpath <- ARpath
+#=======
+AKpath <- "/Users/arek/Documents/GitHub/mowpartboost/"
+ADpath <- "/qarr/projects/studia/mow/mowpartboost/"
+rootdirpath <- ARpath
+#>>>>>>> 4e2247dc75438dbaf9d22875ca4aa933c4aadb43:_rootdirpath.R
+setwd(rootdirpath)
